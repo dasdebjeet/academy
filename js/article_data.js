4ffbe368e7148c856bb2b9466eeb5a33e177bdba
Debjeet Das <debjeet194@gmail.com>
2022-08-24 23:47:30 +0530

    Modified -- Added and updated files

---
+++ js/article_data.js
@@ -0,0 +1,17 @@
+var article_data = {
+    'article': [{
+        "article_id": "ML01",
+        "article_name": "Linear Regression using area price dataset",
+        "article_tags": ["Linear Regression"],
+        "author": "Debjeet Das",
+        "author_image_name": "author_debjeetdas.jpg",
+        "article_published_date": "23.08.2022"
+    }, {
+        "article_id": "ML02",
+        "article_name": "Linear Regression using canada per capita income dataset",
+        "article_tags": ["Linear Regression"],
+        "author": "Debjeet Das",
+        "author_image_name": "author_debjeetdas.jpg",
+        "article_published_date": "24.08.2022"
+    }]
+}
