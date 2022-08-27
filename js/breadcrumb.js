b1e05bea720ad9427572bad87e2813c56edcc6e2
Debjeet Das <debjeet194@gmail.com>
2022-08-27 23:26:32 +0530

    Modified -- Updated breadcrumb link fully

--- js/breadcrumb.js
+++ js/breadcrumb.js
@@ -5,18 +5,22 @@ for (const breadcrumb_link of breadcrumb_links) {
         var link = $(breadcrumb_link).text()
         var link_arr = link.split(" ")
 
+        if (breadcrumb_arr[breadcrumb_arr.length - 1] != link) {
+            for (var i = 0; i < link_arr.length; i++) {
+                if (i == 0) link = link_arr[i].toLowerCase()
+                else link += link_arr[i]
+            }
 
-        for (var i = 0; i < link_arr.length; i++) {
-            if (i == 0) link = link_arr[i].toLowerCase()
-            else link += link_arr[i]
+
+            if (link == "catagories") {
+                window.location.replace("../../../catagories.html");
+            } else if (link == "home") {
+                window.location.replace("../../../index.html");
+            } else {
+                window.location.replace("../../" + link + ".html");
+            }
         }
 
-        if (link == "catagories") {
-            window.location.replace("../../../catagories.html");
-        } else if (link == "home") {
-            window.location.replace("../../../index.html");
-        } else {
-            window.location.replace("../../" + link + ".html");
-        }
+
     })
 }
